a7781fa4e0bb236628662fac1bad27ebb50eb05b
VladaBolotova <vladislavabolotova@gmail.com>
2022-12-07 15:37:02 -0500

    engineer function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer")
+const Intern = require("./lib/intern")
 const inquirer = require('inquirer');
 const path = require ('path');
 
@@ -47,5 +49,89 @@ function menu(){
         message: "Do you want to keep building your team?",
         choices: ["building engineer", "building intern", "finish building my team"],
         type:"list",
+    }).then(function choice(){
+        switch(choice.buildTeam){
+            case "building engineer":
+                addEngineer();
+            break;
+            case "building intern":
+                addIntern();
+            break;
+            default:
+                createTeam();
+        }
     })
+};
+
+const addEngineer =()=> {
+    inquirer.prompt([
+        {
+            name:"name",
+            message: "What is the name of enginneer?",
+            type:"input",
+        },
+        {
+            name:"id",
+            message: "What is the id of engineer?",
+            type:"input",
+        },
+        {
+            name:"email",
+            message: "What is the email of engineer?",
+            type:"input",
+        },
+        {
+            name:"gitHub",
+            message: "What is your GitHub of engineer?",
+            type:"input",
+        },
+    ]).then((res)=> {
+    
+        const engineer = new Engineer(res.name, res.id, res.email, res.gitHub);
+        menu();
+        
+})
+}
+
+const addIntern =()=> {
+    inquirer.prompt([
+        {
+            name:"name",
+            message: "What is the name of intern?",
+            type:"input",
+        },
+        {
+            name:"id",
+            message: "What is the id of intern?",
+            type:"input",
+        },
+        {
+            name:"email",
+            message: "What is the email of intern?",
+            type:"input",
+        },
+        {
+            name:"school",
+            message: "What is the school of intern?",
+            type:"input",
+        },
+    ]).then((res)=> {
+    
+        const intern = new Intern(res.name, res.id, res.email, res.school);
+        menu();
+        
+})
+}
+
+const createTeam =()=>{
+
+    const html = [];
+
+
+
+
+
+    
+    fs.writeFile('index.html', html, (err)=>
+        err ? console.error(err) : console.log('Commit logged'));
 }
